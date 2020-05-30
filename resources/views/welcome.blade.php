db83e190bed7c5a5b391f21ab6511ef4e2921c06
ironcodecorp <pablitouy@gmail.com>
2020-05-30 18:11:17 -0300

    Iniciando Front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,51 +100,16 @@
                         <div class="container">
                     
                           <div class="row">
-                            <div class="col-md-4">
-                              <div class="card mb-4 shadow-sm">
-                                <img src="https://picsum.photos/seed/picsum/200/300">
-                                <div class="card-body">
-                                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                  <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            <div class="col-md-12">
+                                <div class="jumbotron">
+                                    <div class="container">
+                                      <h1 class="display-3">Bienvenidos</h1>
+                                      <h3>Esta plataforma esta construida para poder donar o comprar a través de una forma de subasta aquello que no estamos usando, todo lo recaudado va a ser usado en acciones que tengan un impacto positivo en nuestro planeta y comunidad.
+                                        Más informacion en la seccion de about</h3>
                                     </div>
-                                    <small class="text-muted">9 mins</small>
                                   </div>
-                                </div>
-                              </div>
                             </div>
-                            <div class="col-md-4">
-                              <div class="card mb-4 shadow-sm">
-                                <img src="https://picsum.photos/seed/picsum/200/300">
-                                <div class="card-body">
-                                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                  <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                    </div>
-                                    <small class="text-muted">9 mins</small>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                            <div class="col-md-4">
-                              <div class="card mb-4 shadow-sm">
-                                <img src="https://picsum.photos/seed/picsum/200/300">
-                                <div class="card-body">
-                                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                  <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                    </div>
-                                    <small class="text-muted">9 mins</small>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
+                            
                     
                             <div class="col-md-4">
                               <div class="card mb-4 shadow-sm">
