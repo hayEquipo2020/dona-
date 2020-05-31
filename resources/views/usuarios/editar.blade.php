f27176763f04f95875ccb3e12ad9dad45b43bf88
cgherzo <carolinagherzo@gmail.com>
2020-05-30 22:56:10 -0300

    Fixing merge 2

--- resources/views/usuarios/editar.blade.php
+++ resources/views/usuarios/editar.blade.php
@@ -20,14 +20,7 @@
             <div class="card">
                 <div class="card-body">
                     <form>
-                    <div class="card-header">Perfil de Usuario</div>
-                    <ul class="list-group">
-                    <li class="list-group-item">Nombre: <strong> {{ Auth::user()->name }}</strong></li>
-                    <li class="list-group-item">E-Mail: <strong> {{ Auth::user()->email }}</strong></li>
-                    <li class="list-group-item">Direccion: <strong> {{ Auth::user()->direccion }}</strong></li>
-                    <li class="list-group-item">Telefono: <strong>  {{ Auth::user()->telefono }}</strong></li>
-                    <li class="list-group-item">Pais: <strong> {{ Auth::user()->pais }}</strong></li>
-                    </ul>
+
                     </form>
                 </div>
             </div>
